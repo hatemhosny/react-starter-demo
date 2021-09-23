534718ebeec82f1b4fb98f259b5ac45dcc8ea882
Hatem Hosny <hatemhosny@users.noreply.github.com>
2021-09-23 20:53:58 +0200

    try qiankun

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 function App(props) {
@@ -14,4 +23,28 @@ function App(props) {
             " times."),
         React.createElement("button", { onClick: () => setCount(count + 1) }, "Click me")));
 }
-ReactDOM.render(React.createElement(App, { name: "React" }), document.querySelector("#app"));
+function render(props) {
+    const { container } = props;
+    ReactDOM.render(React.createElement(App, Object.assign({}, props)), container ? container.querySelector('#app') : document.querySelector('#app'));
+}
+if (!window.__POWERED_BY_QIANKUN__) {
+    render({});
+}
+export function bootstrap() {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log('[react16] react app bootstraped');
+    });
+}
+export function mount(props) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log('[react16] props from main framework', props);
+        render(props);
+    });
+}
+export function unmount(props) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const { container } = props;
+        ReactDOM.unmountComponentAtNode(container ? container.querySelector('#app') : document.querySelector('#app'));
+    });
+}
+render({ name: 'React' });
--- src/script.jsx
+++ src/script.jsx
@@ -13,4 +13,29 @@ function App(props) {
     </div>
   );
 }
-ReactDOM.render(<App name="React" />, document.querySelector("#app"));
+
+
+function render(props) {
+  const { container } = props;
+  ReactDOM.render(<App {...props} />, container ? container.querySelector('#app') : document.querySelector('#app'));
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  render({});
+}
+
+export async function bootstrap() {
+  console.log('[react16] react app bootstraped');
+}
+
+export async function mount(props) {
+  console.log('[react16] props from main framework', props);
+  render(props);
+}
+
+export async function unmount(props) {
+  const { container } = props;
+  ReactDOM.unmountComponentAtNode(container ? container.querySelector('#app') : document.querySelector('#app'));
+}
+
+render({name: 'React'});
